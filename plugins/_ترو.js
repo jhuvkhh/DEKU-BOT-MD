0bb5deb331144120e3ef6d6acb8c727bcb15a657
jhuvkhh <syzralrfay@gmail.com>
2024-07-24 01:56:58 +0300

    Update _ترو.js

--- plugins/_ترو.js
+++ plugins/_ترو.js
@@ -1,6 +1,6 @@
 
 
-const canal2 = 'https://telegra.ph/file/078014e17aedf4f7a6cd9.jpg'; 
+const canal2 = 'https://whatsapp.com/channel/0029VaJxI9uJkK7BedTH0D11'; 
 // COMBINACIÓN DE MENSAJES
 // Adaptar el simple.js
 let handler = async (m, { conn, usedPrefix, command, text }) => {
@@ -10,39 +10,39 @@ let handler = async (m, { conn, usedPrefix, command, text }) => {
 const sections = [{
 title: `Título de la sección`,
 rows: [
-{ header: 'Encabezado1', title: "Título1", description: 'Descripción1', id: usedPrefix + "menu" }, 
-{ header: 'Encabezado2', title: "Título2", description: 'Descripción2', id: "Id2" }, 
-{ header: 'Encabezado3', title: "Título3", description: 'Descripción3', id: "Id3" }, 
-{ header: 'Encabezado4', title: "Título4", description: 'Descripción4', id: "Id4" }, 
+{ header: 'الخيار 1', title: "Título1", description: 'الوصف 1', id: usedPrefix + "menu" }, 
+{ header: 'الخيار 2', title: "Título2", description: 'الوصف2', id: "play" }, 
+{ header: 'الخيار3', title: "Título3", description: '3 الخيار', id: "play2" }, 
+{ header: 'Encabezado4', title: "Título4", description: 'Descripción4', id: "بين" }, 
 ]},]  
 const messages = [[ // CARRUSEL 1
 'Descripción de Carrusel 1', 
 'Footer de Carrusel 1',
-'https://telegra.ph/file/24b24c495b5384b218b2f.jpg',
-[['Botón1', usedPrefix + 'menu'], ['Botón2', 'Id2'] /* etc... */],
-[['Texto para copiar 1'], ['Texto para copiar 2'] /* etc... */],
-[['Enlace1', canal2], ['Enlace2', 'https://example.com/link2'] /* etc... */],
+'https://telegra.ph/file/547c1e749ac2d3f12320b.jpg',
+[['.menu', usedPrefix + 'menu'], ['.اوامر', 'menu'] /* etc... */],
+[['.بين'], ['.play'] /* etc... */],
+[['لنك القناه', canal2], ['Enlace2', 'https://telegra.ph/file/547c1e749ac2d3f12320b.jpg'] /* etc... */],
 [['Botón Lista 1', sections], ['Botón Lista 2', sections] /* etc... */]
 ], [ // CARRUSEL 2
 'Descripción de Carrusel 2',
 'Footer de Carrusel 2',
-'https://telegra.ph/file/e9239fa926d3a2ef48df2.jpg',
+'https://https://telegra.ph/file/04c90789c67308398c3a3.jpg',
 [['Botón1', 'Id1'], ['Botón2', 'Id2']],
 [['Texto para copiar 1'], ['Texto para copiar 2']],
-[['Enlace1', 'https://example.com/link1'], ['Enlace2', 'https://example.com/link2']],
+[['Enlace1', 'https://example.com/link1'], ['Enlace2', 'https://whatsapp.com/channel/0029VaJxI9uJkK7BedTH0D11']],
 [['Botón Lista 1', sections], ['Botón Lista 2', sections]]
 ], [ // CARRUSEL 3
 'Descripción de Carrusel 3',
 'Footer de Carrusel 3',
-'https://telegra.ph/file/ec725de5925f6fb4d5647.jpg',
+'https://telegra.ph/file/2adc31f08bb602e9a3357.jpg',
 [['Botón1', 'Id1'], ['Botón2', 'Id2']],
 [['Texto para copiar 1'], ['Texto para copiar 2']],
-[['Enlace1', 'https://example.com/link1'], ['Enlace2', 'https://example.com/link2']],
+[['لنك القناه 🔥', 'https://whatsapp.com/channel/0029VaJxI9uJkK7BedTH0D11'], ['لنك القناه 🔥', 'https://whatsapp.com/channel/0029VaJxI9uJkK7BedTH0D11']],
 [['Botón Lista 1', sections], ['Botón Lista 2', sections]]
 ], [ // CARRUSEL 4
 'Descripción de Carrusel 4',
 'Footer de Carrusel 4',
-'https://telegra.ph/file/7acad0975febb71446da5.jpg',
+'https://telegra.ph/file/b3873e69a8a8a1b1ce478.jpg',
 [['Botón1', 'Id1'], ['Botón2', 'Id2']],
 [['Texto para copiar 1'], ['Texto para copiar 2']],
 [['Enlace1', 'https://example.com/link1'], ['Enlace2', 'https://example.com/link2']],
