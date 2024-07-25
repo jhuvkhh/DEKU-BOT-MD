ead3db6abacffe5e9eb302706947e29ee684e548
jhuvkhh <syzralrfay@gmail.com>
2024-07-25 17:47:35 +0300

    Update info-speedtest.js

--- plugins/info-speedtest.js
+++ plugins/info-speedtest.js
@@ -1,28 +1,29 @@
-import cp from 'child_process';
-import { promisify } from 'util';
-const exec = promisify(cp.exec).bind(cp);
+import cp from 'child_process'
+import {promisify} from 'util'
 
-const handler = async (m) => {
-    let o;
-    try {
-        o = await exec('python3 speed.py --secure --share');
-        const {stdout, stderr} = o;
-        if (stdout.trim()) {
-            const match = stdout.match(/http[^"]+\.png/);
-            const urlImagen = match ? match[0] : null;
-            await conn.sendMessage(m.chat, {image: {url: urlImagen}, caption: stdout.trim()}, {quoted: m});
-        }
-        if (stderr.trim()) { 
-            const match2 = stderr.match(/http[^"]+\.png/);
-            const urlImagen2 = match2 ? match2[0] : null;    
-            await conn.sendMessage(m.chat, {image: {url: urlImagen2}, caption: stderr.trim()}, {quoted: m});
-        }
-    } catch (e) {
-        o = e.message;
-        return m.reply(o)
-    }
-};
-handler.help = ['speedtest'];
-handler.tags = ['info'];
-handler.command = /^(سرعه?|test?speed)$/i;
-export default handler;
+const exec = promisify(cp.exec).bind(cp)
+
+var handler = async (m) => {
+
+let o
+  
+try {
+
+o = await exec('python3 speed.py --secure --share');
+} catch (e) {
+o = e
+} finally {
+let {stdout, stderr} = o
+
+
+if (stdout.trim()) conn.reply(m.chat, stdout, m)
+if (stderr.trim()) conn.reply(m.chat, stderr, m)
+}
+
+}
+handler.help = ['speedtest']
+handler.tags = ['bot']
+handler.command = /^(speedtest?|test?speed|سيرفر)$/i
+
+
+export default handler
