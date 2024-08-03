93fa67ef42403eb9b5b6c395b816657f4dd880a0
hgylh <anawmnn014@gmail.com>
2024-08-03 08:22:30 +0300

    Create ترجمه-ادوات.js

---
+++ plugins/ترجمه-ادوات.js
@@ -0,0 +1,40 @@
+import translate from '@vitalets/google-translate-api'
+const defaultLang = 'ar'
+const tld = 'cn'
+
+let handler = async (m, { args, usedPrefix, command }) => {
+    let err = `
+𝐆𝐎𝐉𝐎⚡𝐁𝐎𝐓 \n https://whatsapp.com/channel/0029VaJxI9uJkK7BedTH0D11
+         
+
+*امثله الترجمه*
+
+.ترجم gojo satoro
+.ترجم aha
+  
+
+
+https://cloud.google.com/translate/docs/languages
+`.trim()
+
+    let lang = args[0]
+    let text = args.slice(1).join(' ')
+    if ((args[0] || '').length !== 2) {
+        lang = defaultLang
+        text = args.join(' ')
+    }
+    if (!text && m.quoted && m.quoted.text) text = m.quoted.text
+
+    try {
+       let result = await translate(text, { to: lang, autoCorrect: true }).catch(_ => null) 
+       m.reply(result.text)
+    } catch (e) {
+        throw err
+    } 
+
+}
+handler.help = ['trad <leng> <text>']
+handler.tags = ['tools']
+handler.command = ['tl', 'ترجم']
+
+export default handler
